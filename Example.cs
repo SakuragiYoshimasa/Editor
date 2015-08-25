7694e054c2495f567faa0098d70be8e2ed7389ec
櫻木善将 <ysrhsp@outlook.com>
2015-08-25 14:18:30 +0900

    initial commit

---
+++ Example.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+class Example  {
+	[InitializeOnLoadMethod]
+	static void Exp(){
+
+		EditorApplication.hierarchyWindowItemOnGUI += OnGUI;
+
+	}
+
+	static void OnGUI(int instanceID,Rect selectionRect){
+
+		var obj = EditorUtility.InstanceIDToObject (instanceID);
+		var go = obj as GameObject;
+
+		if(go == null){
+			return;
+		}
+
+		var pos = selectionRect;
+		pos.x = pos.xMax - 16;
+		pos.width = 16;
+		var newActive = GUI.Toggle (pos,go.activeSelf,string.Empty);
+		go.SetActive (newActive);
+
+	}
+}
